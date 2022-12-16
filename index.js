75fcc1e95aab7e96eda638d0f11b67bb5de6c7f5
Jesse Eisenberg <sosaucily@gmail.com>
2022-12-16 10:39:13 +0100

    remove-ports-no-longer-needed
    remove-ports-no-longer-needed

--- index.js
+++ index.js
@@ -9,31 +9,20 @@ const app = express();
 const PORT = 3999;
 const HOST = "localhost";
 const NODE_1 = "http://stx-btc1.dlc.link";
-const NODE_2 = "http://electrs-btc2.dlc.link";
 
 // Logging
 app.use(morgan('dev'));
 
 // Proxy endpoints
 app.use('/', createProxyMiddleware({
-    target: NODE_1,
-    changeOrigin: true,
-    router: {
-      'localhost:3999' : NODE_1 + ':3999',
-      'localhost:8000' : NODE_1 + ':8000',
-      'localhost:3004' : NODE_2 + ':3004',
-    }
+  target: NODE_1,
+  changeOrigin: true,
+  router: {
+    'localhost:3999': NODE_1 + ':' + PORT,
+  }
 }));
 
 // Start Proxy
 app.listen(PORT, HOST, () => {
-    console.log(`Starting Proxy at ${HOST}:${PORT}`);
-});
-
-app.listen(3004, HOST, () => {
-
-});
-
-app.listen(8000, HOST, () => {
-
+  console.log(`Starting Proxy at ${HOST}:${PORT}`);
 });
